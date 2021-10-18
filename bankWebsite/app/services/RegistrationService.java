6fcef07e1b22bb627af2e5db6a79f096d2ef8448
nexxoshivank <shivank.sharma@nexxo.com>
2021-10-18 11:07:55 +0530

    made more validations in Registration service

--- bankWebsite/app/services/RegistrationService.java
+++ bankWebsite/app/services/RegistrationService.java
@@ -11,7 +11,6 @@ import models.UserDetails;
 import models.UserPasswords;
 import org.joda.time.DateTime;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
@@ -22,56 +21,74 @@ public class RegistrationService {
 		RegistrationResponse registrationResponse = new RegistrationResponse();
 		
 		try {
-			System.out.println(registrationRequest.getAadhaarNumber());
-			boolean aadhar = Pattern.matches("[0-9]{12}",registrationRequest.getAadhaarNumber());
-			///boolean email  = Pattern.matches("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$",registrationRequest.getEmail());
-			boolean pan = Pattern.matches("[a-zA-Z0-9]{8}",registrationRequest.getPanNumber());
-			
-			if (aadhar  && pan) {
-				
-				UserDetails userDetails = new UserDetails();
-				
-				userDetails.setFirstName(registrationRequest.getFirstName());
-				userDetails.setLastName(registrationRequest.getLastName());
-				userDetails.setAadhaarNumber(registrationRequest.getAadhaarNumber());
-				userDetails.setPanNumber(registrationRequest.getPanNumber());
-				userDetails.setMobile(registrationRequest.getMobile());
-				userDetails.setEmail(registrationRequest.getEmail());
-				userDetails.setCreatedOn(new DateTime());
-				userDetails.setDob(registrationRequest.getDob());
-				
-				userDetails.setRoleId(Roles.getInstance(Roles.roleTypeEnum.CUSTOMER.getRoleId()));
-				userDetails.setStatusId(Statuses.getInstanceof(Statuses.statusEnum.ACTIVE.getId()));
-				
-				
-				UserPasswords userPasswords = new UserPasswords();
-				userPasswords.setDateTime(new DateTime());
-				userPasswords.setUserId(userDetails);
-				userPasswords.setPass(registrationRequest.getPassword());
-				
-				
-				UserDao.saveUserDetails(userDetails);
-				PasswordDao.saveUserPasswords(userPasswords);
-				registrationResponse.setStatusCode(200);
-				registrationResponse.setStatusName("User Registered");
-				
-			}
-			else {
-				registrationResponse.setStatusName("enter correct format of each field");
+			boolean fname = Pattern.matches("^[A-Za-z]{3,29}$", registrationRequest.getFirstName());
+			if (!fname) {
+				registrationResponse.setStatusName("please enter correct first name");
+				return registrationResponse;
 			}
 			
+			boolean lname = Pattern.matches("^[A-Za-z]{3,29}$", registrationRequest.getLastName());
+			if (!lname) {
+				registrationResponse.setStatusName("please enter correct last name");
+				return registrationResponse;
+			}
+			boolean mobile = Pattern.matches("^[6-9]\\d{9}$", registrationRequest.getMobile());
+			if (!mobile) {
+				registrationResponse.setStatusName("please enter correct mobile number");
+				return registrationResponse;
+			}
+			boolean email = Pattern.matches("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$", registrationRequest.getEmail());
+			if (!email) {
+				registrationResponse.setStatusName("please enter correct email");
+				return registrationResponse;
+			}
 			
-		}
-		catch (DuplicateKeyException duplicateKeyException){
+			boolean aadhaar = Pattern.matches("^[2-9]{1}[0-9]{3}[0-9]{4}[0-9]{4}$", registrationRequest.getAadhaarNumber());
+			if (!aadhaar) {
+				registrationResponse.setStatusName("please enter correct aadhaar number");
+				return registrationResponse;
+			}
+			
+			boolean pan = Pattern.matches("[A-Z]{5}[0-9]{4}[A-Z]", registrationRequest.getPanNumber());
+			if (!pan) {
+				registrationResponse.setStatusName("please enter correct pan number");
+				return registrationResponse;
+			}
+			
+			UserDetails userDetails = new UserDetails();
+			
+			userDetails.setFirstName(registrationRequest.getFirstName());
+			userDetails.setLastName(registrationRequest.getLastName());
+			userDetails.setAadhaarNumber(registrationRequest.getAadhaarNumber());
+			userDetails.setPanNumber(registrationRequest.getPanNumber());
+			userDetails.setMobile(registrationRequest.getMobile());
+			userDetails.setEmail(registrationRequest.getEmail());
+			userDetails.setCreatedOn(new DateTime());
+			userDetails.setDob(registrationRequest.getDob());
+			
+			userDetails.setRoleId(Roles.getInstance(Roles.roleTypeEnum.CUSTOMER.getRoleId()));
+			userDetails.setStatusId(Statuses.getInstanceof(Statuses.statusEnum.ACTIVE.getId()));
+			
+			
+			UserPasswords userPasswords = new UserPasswords();
+			userPasswords.setDateTime(new DateTime());
+			userPasswords.setUserId(userDetails);
+			userPasswords.setPass(registrationRequest.getPassword());
+			
+			UserDao.saveUserDetails(userDetails);
+			PasswordDao.saveUserPasswords(userPasswords);
+			registrationResponse.setStatusCode(200);
+			registrationResponse.setStatusName("User Registered");
+			
+			
+		} catch (DuplicateKeyException duplicateKeyException) {
 			registrationResponse.setStatusCode(500);
 			registrationResponse.setStatusName("Pan, Aadhaar or email already registered");
 			return registrationResponse;
-		}
-		
-		catch (Exception exception) {
+		} catch (Exception exception) {
 			
 			registrationResponse.setStatusCode(500);
-			registrationResponse.setStatusName("error1");
+			registrationResponse.setStatusName("error1" + exception);
 			return registrationResponse;
 		}
 		
